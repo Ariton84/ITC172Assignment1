00391feea3673c7f0fec9ed767fb0f9026300ada
Ariton84 <vanver@gmail.com>
2017-01-09 12:04:33 -0800

    Add files via upload

---
+++ Assignment1/Default.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+         {
+         string[] tipPercent = { "Ten Percent", "Fifteen Percent", "Twenty Percent", "Other" };
+         TipPercentsRBL.DataSource = tipPercent;
+         TipPercentsRBL.DataBind();
+         }
+    }
+
+    protected void SubmitButton_Click(object sender, EventArgs e)
+    {
+        GetInfo();
+    }
+
+   protected void GetInfo()
+    {
+        Tip tip = new Tip();
+        tip.MealAmount = double.Parse(MealTextBox.Text);
+        if (OtherTextBox.Text == "")
+        {
+            foreach (ListItem item in TipPercentsRBL.Items)
+            {
+                tip.TipPercent = 0;
+                if (item.Selected)
+                {
+                    if (item.Text.Equals("Ten Percent"))
+                    {
+                        tip.TipPercent = .1;
+                    }
+                    else if (item.Text.Equals("fifteen Percent"))
+                    {
+                        tip.TipPercent = .15;
+                    }
+                    else if (item.Text.Equals("Twenty Percent"))
+                    {
+                        tip.TipPercent = .2;
+                    }
+                }
+            }
+        }
+        else
+        {
+            tip.TipPercent = double.Parse(OtherTextBox.Text);
+        }
+
+        ResultsLabel.Text = "Amount: " + tip.MealAmount.ToString() + "<br/>" + "Tip: "
+            + tip.CalculateTip().ToString() + "<br/>" +
+            "Tax: " + tip.CalculateTax().ToString() + "<br/>" +
+            "Total: " + tip.CalculateTotal().ToString();
+    }
+}
